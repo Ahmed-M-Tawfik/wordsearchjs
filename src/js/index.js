166cd5e45684892dd338f3cefd4d38296fd499c5
Ahmed Tawfik <a.tawfik@elsevier.com>
2023-11-12 01:30:27 +0000

    Build table grid programmatically. Prepping for migrating to js module

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,29 @@
-window.onload = function () {
-    for (let node of document.querySelectorAll("td")) {
+function init() {
+    let horizontal = 8;
+    let vertical = 8;
 
-        node.onclick = function () {
-            node.classList.toggle("selected");
+    let gridTable = document.getElementById("gameBoardTable");
+    for(let i = 0; i < vertical; i++) {
+        const row = document.createElement("tr");
+        for(let j = 0; j < horizontal; j++) {
+            const td = document.createElement("td");
+            td.textContent = generateRandomCharacter();
+            td.onclick = getSelectionToggleFunc(td);
+            row.appendChild(td);
         }
-
-        if (node.textContent !== "") continue;
-        let charCode = Math.round(65 + Math.random() * 25);
-        node.textContent = String.fromCharCode(charCode);
+        gridTable.appendChild(row);
     }
 }
+
+function generateRandomCharacter() {
+    let charCode = Math.round(65 + Math.random() * 25);
+    return String.fromCharCode(charCode);
+}
+
+function getSelectionToggleFunc(node) {
+    return function () {
+        node.classList.toggle("selected");
+    }
+}
+
+init();
